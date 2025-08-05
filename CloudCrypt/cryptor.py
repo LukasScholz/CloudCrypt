95476ec36d97e86bb5990fb6c33da08cba4d0ea1
Lukas Scholz <l.kas.scholz@gmail.com>
2025-08-05 16:42:05 +0200

    added cryptor class

---
+++ CloudCrypt/cryptor.py
@@ -0,0 +1,69 @@
+from cryptography.fernet import Fernet, MultiFernet
+import os.path
+
+
+class Encryption:
+
+    def __init__(self, keyfile):
+        self.keyring = keyfile
+        self._init_key(keyfile)
+
+    def _init_key(self, keyfile):
+        if os.path.exists(keyfile):
+            return
+        # create empty binary file
+        with open(keyfile, 'wb') as f:
+            f.write(b"")
+
+    def _get_keys(self):
+        with open(self.keyring, 'rb') as file:
+            lines = file.readlines()
+        keys = []
+        for line in lines:
+            keys.append(Fernet(line))
+        return MultiFernet(keys)
+
+    def add_key(self):
+        new_key = Fernet.generate_key()
+        with open(self.keyring, 'ab') as f:
+            f.write(new_key)
+            f.write(str.encode("\n"))
+
+    def add_key_to_front(self):
+        new_key = Fernet.generate_key() + str.encode("\n")
+        with open(self.keyring, 'rb') as file:
+            lines = file.readlines()
+        keys = [new_key]
+        for line in lines:
+            keys.append(line)
+        with open(self.keyring, 'wb') as f:
+            for key in keys:
+                f.write(key)
+
+    def encrypt(self, file):
+        fernet = self._get_keys()
+        with open(file, 'rb') as data:
+            original = data.read()
+        encrypted = fernet.encrypt(original)
+        os.remove(file)
+        with open(file+".cyacrypt", 'wb') as data:
+            data.write(encrypted)
+
+    def decrypt(self, file):
+        fernet = self._get_keys()
+        with open(file, 'rb') as data:
+            encrypted = data.read()
+        decrypted = fernet.decrypt(encrypted)
+        os.remove(file)
+        with open(file[0:-9], 'wb') as data:
+            data.write(decrypted)
+
+
+# test
+tempfile = "Mietbescheinigung.pdf"
+e = Encryption("tempkeys.keys")
+#e.add_key()
+#e.encrypt(tempfile)
+#e.decrypt(tempfile)
+e.add_key_to_front()
+
