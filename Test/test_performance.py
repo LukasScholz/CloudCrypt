a0a009bb19d29671cfadd4733fb0d99924343ed3
lukas <l.kas.scholz@gmail.com>
2025-08-27 23:07:30 +0200

    fixed Performance Tests

--- Test/test_performance.py
+++ Test/test_performance.py
@@ -1,5 +1,6 @@
 import random
 import string
+import unittest
 from timeit import default_timer as timer
 
 
@@ -11,32 +12,31 @@ CONFIGPATH = "Test/testconfig.csv"
 FILEAMOUNT = 300
 FILESIZE = 500000
 
-
 def generate_big_random_file(filename, size):
     chars = ''.join([random.choice(string.ascii_letters) for i in range(size)])
     with open(filename, 'w') as f:
         f.write(chars)
 
+class MyTestCase(unittest.TestCase):
+    def test_storage_performance(self):
+        config = Config(CONFIGPATH)
+        loader = Loader(CONFIGPATH)
+        print("Generating Files...")
+        for i in range(FILEAMOUNT):
+            generate_big_random_file(config.LocalStorage + "/tempfile_"+str(i), FILESIZE)
+        print("Creating Storage...")
 
-def main():
-    config = Config(CONFIGPATH)
-    loader = Loader(CONFIGPATH)
-    print("Generating Files...")
-    for i in range(FILEAMOUNT):
-        generate_big_random_file(config.LocalStorage + "/tempfile_"+str(i), FILESIZE)
-    print("Creating Storage...")
-
-    start = timer()
-    loader.create_storage()
-    end = timer()
-    print()
-    print("---------------------------------")
-    print("Time in seconds: "+str(end - start))
-    print("Time per File: "+str((end - start)/FILEAMOUNT))
-    print("Time per Character: " + str((end - start) / (FILEAMOUNT*FILESIZE)))
-    print("---------------------------------")
-    print()
+        start = timer()
+        loader.create_storage()
+        end = timer()
+        print()
+        print("---------------------------------")
+        print("Time in seconds: "+str(end - start))
+        print("Time per File: "+str((end - start)/FILEAMOUNT))
+        print("Time per Character: " + str((end - start) / (FILEAMOUNT*FILESIZE)))
+        print("---------------------------------")
+        print()
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    unittest.main()
