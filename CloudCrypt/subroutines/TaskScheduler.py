8d4af13a6fe9e66a1f8375e96c78db147aec7982
lukas <l.kas.scholz@gmail.com>
2025-09-02 20:35:06 +0200

    Revert "Added linux alternative"
    This reverts commit dd0995626583ba4fb39d012409d33619e6550443.

--- CloudCrypt/subroutines/TaskScheduler.py
+++ CloudCrypt/subroutines/TaskScheduler.py
@@ -4,7 +4,6 @@ from CloudCrypt.subroutines.Exceptions import InvalidArgumentException
 
 
 class WindowsScheduler:
-    ## all functions must be run as administrator
 
     def __init__(self):
         self.intervals = ("MINUTE", "HOURLY", "DAILY", "WEEKLY", "MONTHLY", "ONSTART", "ONLOGON", "ONIDLE")
@@ -20,33 +19,3 @@ class WindowsScheduler:
 
         # All arguments are valid!
         os.system(f'SchTasks /Create /SC {interval} /TN {name} /TR {path} /ST {time}')
-
-    def remove_task(self, name: str):
-        # assume that the name is associated with an existing task
-        os.system(f'SchTasks /DELETE /TN {name}')
-
-class LinuxScheduler:
-    ## uses crontab for scheduling
-
-    # Cron line specifics
-    #   * * * * * "command to be executed"
-    #   - - - - -
-    #   | | | | |
-    #   | | | | ----- Day of week (0 - 7) (Sunday=0 or 7)
-    #   | | | ------- Month (1 - 12)
-    #   | | --------- Day of month (1 - 31)
-    #   | ----------- Hour (0 - 23)
-    #   ------------- Minute (0 - 59)
-
-    def __init__(self):
-        pass
-
-    def create_crontab(self, path):
-        croncmd = path # Todo add output routing?
-        cronline = f"* * * * * {croncmd}" # Todo: add arguments to fix * placeholders
-        os.system(f'( crontab -l | grep -v -F {croncmd} ; echo {cronline} ) | crontab -')
-        # Todo: check Arguments!
-
-    def remove_crontab(self, path):
-        croncmd = path # Todo add output routing?
-        os.system(f'(crontab - l | grep - v - F {croncmd}) | crontab -')
