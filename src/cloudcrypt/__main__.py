f835c56c98356de0a995e04ba2c4f29ea6710929
lukas <l.kas.scholz@gmail.com>
2025-10-20 15:10:11 +0200

    Add encryption key creation option

--- src/cloudcrypt/__main__.py
+++ src/cloudcrypt/__main__.py
@@ -1,10 +1,11 @@
 import argparse
+import shutil
 import sys
 import pathlib
 
 from cloudcrypt import __version__
 from cloudcrypt.subroutines import ConfigManager
-
+from cloudcrypt.subroutines import cryptor
 
 class MainInterface:
 
@@ -12,11 +13,20 @@ class MainInterface:
         self.arguments = args
         self.config = None  # value set during runtime
 
+    '''
+    Main Program Execution
+    '''
     def run(self):
         if self.arguments.version:
             self.display_version()
         if self.arguments.config:
             self.display_configpath()
+        configpath = pathlib.Path(__file__).parent.resolve() / "etc" / "config.csv"
+        self.config = ConfigManager.Config(configpath)
+        if self.arguments.addkey:
+            self.addkey()
+        if self.arguments.loadkeys is not None:
+            self.loadkeys(self.arguments.loadkeys)
 
     def display_version(self):
         print("CloudCrypt version " + __version__)
@@ -25,6 +35,14 @@ class MainInterface:
     def display_configpath(self):
         print(pathlib.Path(__file__).parent.resolve() / "etc" / "config.csv")
         exit(0)
+
+    def addkey(self):
+        encryptor = cryptor.Encryption(self.config)
+        encryptor.add_key()
+
+    def loadkeys(self, newkeys):
+        keyfile = self.config.KeyFile
+        shutil.copyfile(newkeys, keyfile)
 
 
 class CustomArgumentParser(argparse.ArgumentParser):
@@ -37,12 +55,13 @@ class CustomArgumentParser(argparse.ArgumentParser):
 def argparse():
     parser = CustomArgumentParser(description="Encrypted Backup-creator, mainly for Cloud usage")
 
-    # required arguments
-    parser.add_argument("--initkey", "-k", help="Generate a new key File")
-    parser.add_argument("--loadkeys", "-l", help="Load keys from existing Keyfile")
-    parser.add_argument("----verifysetup", help="Verify Setup of Config File")
-
+    # arguments without value
+    parser.add_argument("--addkey", "-a", action="store_true", help="Generate a new key File")
+    parser.add_argument("----verifysetup", action="store_true", help="Verify Setup of Config File")
     parser.add_argument("--config", "-c", action="store_true", help="Get config file path")
+
+    # arguments with value
+    parser.add_argument("--loadkeys", "-l", help="Load keys from existing Keyfile")
 
     # optional arguments
     parser.add_argument("--version", "-v", action="store_true", help="prints version info and exit")
