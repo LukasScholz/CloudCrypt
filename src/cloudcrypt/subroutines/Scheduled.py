628913f62bc7597b569a68cad75d65a02f48c2ea
lukas <l.kas.scholz@gmail.com>
2025-11-05 14:22:33 +0100

    added Scheduled Script

---
+++ src/cloudcrypt/subroutines/Scheduled.py
@@ -0,0 +1,18 @@
+# This script is only called by the scheduled tasks
+
+class Scheduled:
+
+    def __init__(self):
+        pass
+
+    def main(self):
+        pass
+
+
+
+
+
+
+if __name__ == "__main__":
+    scheduler = Scheduled()
+    scheduler.main()
