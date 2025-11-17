d61de2a24855fbc135307c2b6a05174bfac6b754
lukas <l.kas.scholz@gmail.com>
2025-11-17 16:26:40 +0100

    Updated to 0.0.4

--- Test/reinstall_package_local.sh
+++ Test/reinstall_package_local.sh
@@ -1,4 +1,10 @@
+exit
 pip uninstall cloudcrypt -y
 rm -r ./dist
 python -m build
 pip install cloudcrypt --no-index --find-links ./dist
+
+
+## Upload
+twine check dist/*
+twine upload -r testpypi dist/*
