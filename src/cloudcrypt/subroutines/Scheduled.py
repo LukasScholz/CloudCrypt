1ebba0aefa27f4df28762ec50956fd8b2d7c2133
Lukas Scholz <l.kas.scholz@gmail.com>
2025-11-23 14:52:01 +0100

    Added scheduled run encryption script

--- src/cloudcrypt/subroutines/Scheduled.py
+++ src/cloudcrypt/subroutines/Scheduled.py
@@ -1,16 +1,22 @@
 # This script is only called by the scheduled tasks
+import pathlib
+
+from cloudcrypt.subroutines import loader
+
+
+def get_configpath():
+    return pathlib.Path(__file__).parent.resolve() / "etc" / "config.csv"
+
 
 class Scheduled:
 
     def __init__(self):
-        pass
+        self.loader = None
 
     def main(self):
-        pass
-
-
-
-
+        configpath = get_configpath()
+        self.loader = loader.Loader(configpath)
+        self.loader.create_storage()
 
 
 if __name__ == "__main__":
